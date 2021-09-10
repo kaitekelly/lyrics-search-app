96e43cccda77ade21fba83ab6d60ff945630c674
kaitekelly <murphy.kaite@gmail.com>
2021-09-10 13:11:40 -0700

    map and html of response data

--- script.js
+++ script.js
@@ -11,7 +11,44 @@ async function searchSongs(term) {
     const res = await fetch(`${apiURL}/suggest/${term}`);
     const data = await res.json();
 
+    console.log(data);
+
     showData(data);
+}
+
+//Show song and artist in DOM
+function showData(data) {
+    // let output = '';
+
+    // data.data.forEach(song => {
+    //     output += `
+    //     <li>
+    //         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+    //         <button class="btn" data-artist="${song.artist.name}"
+    //         data-songtitle="${song.title}">Get Lyrics</button>
+    //     </li>
+    //     `;
+    // });
+
+    // result.innerHTML = `
+    // <ul class="songs">
+    // ${output}
+    // </ul>
+    // `;
+
+    result.innerHTML = `
+    <ul class="songs">
+        ${data.data.map(song => `
+        <li>
+            <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}"
+            data-songtitle="${song.title}">Get Lyrics</button>
+        </li>
+        `)
+    .join('')
+}
+    </ul>
+    `;
 }
 
 //Event Listeners
